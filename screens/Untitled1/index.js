7d2415a50a1d5aae1e343cdab9897c5b051d4241
Jason Kozel <jason.kozel@crowdbotics.com>
2023-04-28 12:52:24 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,15 +1,13 @@
 import { StyleSheet, SafeAreaView } from "react-native";
 
-const Untitled1 = () => {
-  return <SafeAreaView style={styles.safeArea}>
-        
-    </SafeAreaView>;
+const GettingStarted = () => {
+  return <SafeAreaView style={styles.safeArea}></SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%',
+    height: "100%",
     backgroundColor: "#28333F"
   }
 });
-export default Untitled1;
+export default GettingStarted;
